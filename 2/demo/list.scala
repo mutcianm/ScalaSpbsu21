73b085ffd33fed0de1e6b3efcc26547fd39d7cfa
Mikhail Mutcianko <mutcianko.m@gmail.com>
2020-02-27 02:08:56 +0300

    add lecture 2 demo

---
+++ 2/demo/list.scala
@@ -0,0 +1,8 @@
+sealed trait IntList
+
+object IntList {
+  def sum(l: IntList) = ???
+  def tail(l: IntList) = ???
+  def drop(n: Int, l: IntList) = ???
+  def append(a1: IntList, a2: IntList) = ???
+}
