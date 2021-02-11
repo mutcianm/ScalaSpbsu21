3b1a2f39bc13dceb2d2fe224a9f6f34d0581be12
Dmitrii <dhkas@mail.ru>
2021-02-11 20:05:41 +0300

    Tests pass

--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -12,9 +12,26 @@ object TheGame {
   class RepeatingDigitsException extends RuntimeException
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
-  def generateNumberString(length: Int): String = ???
+  def generateNumberString(length: Int, chars: Set[Char] = (('A' to 'Z')++ ('0' to '9')).toSet): String = {
+    val char = chars.iterator.drop(Random.nextInt(chars.size)).next()
+    length match {
+      case 0 => ""
+      case _ => char.toString + generateNumberString(length - 1, chars.diff(Set(char)))
+    }
+  }
 
-  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = ???
+  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = {
+    if (secret.length != userInput.length) throw new WrongNumberLengthException(secret.length, userInput.length)
+
+    if (secret == userInput) {
+      if (userInput.toSet.size != userInput.length) throw new RepeatingDigitsException
+      return Correct(numTries)
+    }
+
+    val bulls = secret.zip(userInput).map {case (a, b) => a == b}.count(x => x)
+    val cows = secret.toSet.intersect(userInput.toSet).size
+    Incorrect(bulls, cows - bulls)
+  }
 
   def main(args: Array[String]): Unit = {
     print("Enter your name: ")
