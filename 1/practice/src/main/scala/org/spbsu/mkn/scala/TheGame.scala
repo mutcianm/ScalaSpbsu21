958573ad1319d9c1682fffc6e60002e3fa8fcb64
sava-cska <sava-cska@mail.ru>
2021-02-11 20:05:21 +0500

    Practice 01 Task

--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -6,19 +6,63 @@ import scala.util.Random
 object TheGame {
 
   sealed trait GuessResult
+
   case class Correct(numTries: Int) extends GuessResult
+
   case class Incorrect(bulls: Int, cows: Int) extends GuessResult
 
   class RepeatingDigitsException extends RuntimeException
+
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
-  def generateNumberString(length: Int): String = ???
+  def generateNumberString(length: Int): String = {
+    val rnd = Random.alphanumeric
+    var genStr = ""
+    var cnt = -1
+    while (genStr.length < length) {
+      cnt += 1
+      val symb = rnd(cnt)
+      if (genStr.indexOf(symb) == -1)
+        genStr += symb
+    }
+    genStr
+  }
 
-  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = ???
+  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = {
+    if (secret.length != userInput.length)
+      throw new WrongNumberLengthException(secret.length, userInput.length)
+    if (secret.toSet.size != secret.length)
+      throw new RepeatingDigitsException
+
+    if (secret == userInput) Correct(numTries)
+    else {
+      var cntCow = 0
+      var cntBull = 0
+      for (i <- 0 until secret.length) {
+        if (secret(i) == userInput(i))
+          cntBull += 1
+        else
+            if (userInput.indexOf(secret(i)) != -1)
+              cntCow += 1
+      }
+      Incorrect(cntBull, cntCow)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    print("Enter your name: ")
-    val name = readLine()
-    println(s"Hello, $name!")
+    println("Enter length of secret string")
+    val length = readLine().toInt
+    val secret = TheGame.generateNumberString(length)
+    var attempts = 0
+    var gameGoOn = true
+    while (gameGoOn) {
+      println("Enter your query")
+      val query = readLine()
+      attempts += 1
+      TheGame.validate(secret, query, attempts) match {
+        case Correct(x) => println(s"You guess string in $x queries"); gameGoOn = false
+        case Incorrect(b, c) => println(s"You have $b bulls and $c cows")
+      }
+    }
   }
 }
