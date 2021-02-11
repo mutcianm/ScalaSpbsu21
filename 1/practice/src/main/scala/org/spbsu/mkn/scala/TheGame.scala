68722cdee48de51aaacae81db6e81a8b5bc117d3
mandelshtamd <Ofwgktadgaf1984>
2021-02-11 19:22:39 +0300

    finished home task

--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -12,13 +12,69 @@ object TheGame {
   class RepeatingDigitsException extends RuntimeException
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
-  def generateNumberString(length: Int): String = ???
+  def generateNumberString(length: Int): String = {
+    val gameAlphabet = ('a' to 'z').toList ++ ('A' to 'Z').toList ++ (0 to 9 ).toList
+    val answer = Random.shuffle(gameAlphabet).take(length)
+    answer.mkString("")
+  }
 
-  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = ???
+  def checkFormat(userInput : String, rightLength : Int): Unit = {
+    if (userInput.length != rightLength)
+      throw new WrongNumberLengthException(rightLength, userInput.length)
+    if (userInput.toSet.size != userInput.length)
+      throw new RepeatingDigitsException
+  }
+
+  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = {
+    checkFormat(userInput, secret.length)
+
+    if (userInput == secret)
+      return Correct(numTries)
+
+    val commonSymbols = secret.toSet.intersect(userInput.toSet)
+    val cows = commonSymbols.count(it => userInput.indexOf(it) != secret.indexOf(it))
+    val bulls = commonSymbols.size - cows
+    Incorrect(bulls, cows)
+  }
+
+  def parseInput() : Int = {
+    print("Введите длину загаданного слова: ")
+    val inputData = readLine()
+    var length = 0
+    length = inputData.toInt
+    length
+  }
+
+  def makeOneTry(secret : String, numTries : Int) : GuessResult = {
+    println("Введите свою догадку: ")
+    val usersTry = readLine()
+    var res : GuessResult = null
+    try {
+      res = validate(secret, usersTry, numTries)
+    } catch {
+      case e : RepeatingDigitsException => println("В вашей попытке были повторяющиеся символы.")
+      case e : WrongNumberLengthException => println(s"Попытка должна быть длины ${secret.length}")
+    }
+    res
+  }
 
   def main(args: Array[String]): Unit = {
-    print("Enter your name: ")
-    val name = readLine()
-    println(s"Hello, $name!")
+    val length = parseInput()
+    val secret = generateNumberString(length)
+
+    var numTries = 0
+    var isTimeToFinish = false
+    while (!isTimeToFinish) {
+      val res = makeOneTry(secret, numTries)
+      numTries += 1
+
+      res match {
+        case res : Correct =>
+          isTimeToFinish = true
+          println(s"Ура, вы угадали $secret с ${res.numTries} попытки")
+        case res : Incorrect => println(s"Пока неверно. Вы угадали ${res.cows} коров и ${res.bulls} быков.")
+        case _ =>
+      }
+    }
   }
 }
