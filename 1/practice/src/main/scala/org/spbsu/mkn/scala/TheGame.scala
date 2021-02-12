9b1331b4d8f246793b226fe5a23408ffdcd3fad8
margarita0303 <lashina.2000@gmail.com>
2021-02-12 20:38:08 +0300

    function generateNumberString added

--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -12,7 +12,15 @@ object TheGame {
   class RepeatingDigitsException extends RuntimeException
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
-  def generateNumberString(length: Int): String = ???
+  def generateNumberString(length: Int): String = {
+    val symbols = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
+    val sb = new StringBuilder
+    for (_ <- 1 to length) {
+      val randomIndex = util.Random.nextInt(symbols.length)
+      sb.append(symbols(randomIndex))
+    }
+    sb.toString
+  }
 
   def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = ???
 
