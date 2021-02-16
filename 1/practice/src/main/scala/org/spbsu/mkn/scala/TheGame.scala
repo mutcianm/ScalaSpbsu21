9fb65013434024d7d08d38a271ee86bcc79dbdc2
margarita0303 <lashina.2000@gmail.com>
2021-02-16 18:26:23 +0300

    main is finished (the game), function tryToGuess added

--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -6,10 +6,13 @@ import scala.util.Random
 object TheGame {
 
   sealed trait GuessResult
+
   case class Correct(numTries: Int) extends GuessResult
+
   case class Incorrect(bulls: Int, cows: Int) extends GuessResult
 
   class RepeatingDigitsException extends RuntimeException
+
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
   def generateNumberString(length: Int): String = {
@@ -27,13 +30,45 @@ object TheGame {
     else {
       val matchingSymbols = userInput.toSet.intersect(secret.toSet).size
       val bulls = userInput.zipWithIndex.count(i => secret.zipWithIndex.contains(i))
-      Incorrect(bulls, matchingSymbols - bulls)
+      val cows = matchingSymbols - bulls
+      Incorrect(bulls, cows)
     }
   }
 
+  var wordIsGuessed = false
+  var numberOfTries = 0
+
+  def tryToGuess(secret: String): GuessResult = {
+    print("Try to guess: ")
+    val userInput = readLine()
+    var result: GuessResult = null
+
+    try {
+      result = validate(secret, userInput)
+      result match {
+        case _: Correct =>
+          println("Right! The number of your tries: " + numberOfTries)
+          wordIsGuessed = true
+        case result: Incorrect =>
+          println("Not right. Bulls: " + result.bulls + ", cows: " + result.cows)
+      }
+    } catch {
+      case _: WrongNumberLengthException => println("Wrong length of your input. Try again.")
+      case _: RepeatingDigitsException => println("Repeating digits in your input. Try again.")
+    }
+    result
+  }
+
   def main(args: Array[String]): Unit = {
-    print("Enter your name: ")
-    val name = readLine()
-    println(s"Hello, $name!")
+    print("Welcome to the game! Enter the length of the word you will guess: ")
+    val length = readLine().toInt
+    val secret = generateNumberString(length)
+
+    while (!wordIsGuessed) {
+      numberOfTries += 1
+      tryToGuess(secret)
+    }
   }
 }
+
+
