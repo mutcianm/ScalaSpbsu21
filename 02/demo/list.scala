d06e3ff23f928e987c27cb45f3d1fbe4c1d250a9
Mikhail Mutcianko <mutcianko.m@gmail.com>
2021-02-17 22:35:20 +0300

    remove obsolete code

--- 02/demo/list.scala
+++
@@ -1,8 +0,0 @@
-sealed trait IntList
-
-object IntList {
-  def sum(l: IntList) = ???
-  def tail(l: IntList) = ???
-  def drop(n: Int, l: IntList) = ???
-  def append(a1: IntList, a2: IntList) = ???
-}
