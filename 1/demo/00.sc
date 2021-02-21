6fd76b32634e12222ab0b87f074ed9f17987f747
a-alex-l <lesnik-a-a@yandex.ru>
2021-02-21 16:14:12 +0300

    Added all :)

--- 1/demo/00.sc
+++ 1/demo/00.sc
@@ -1,5 +1,7 @@
 //todo: What is Scala?
 
+val x = 0
+
 //define method isPrime
 
 //print all primes from 1 to 100
--- 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
+++ 1/practice/src/main/scala/org/spbsu/mkn/scala/TheGame.scala
@@ -12,13 +12,52 @@ object TheGame {
   class RepeatingDigitsException extends RuntimeException
   class WrongNumberLengthException(expected: Int, got: Int) extends RuntimeException
 
-  def generateNumberString(length: Int): String = ???
+  def generateNumberString(length: Int): String = {
+    val initialString = (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')).mkString("")
+    val shuffledString = Random.shuffle(initialString).mkString("")
+    shuffledString.substring(26 + 26 + 10 - length)
+  }
 
-  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = ???
+  def validate(secret: String, userInput: String, numTries: Int = 1): GuessResult = {
+    if (secret.length != userInput.length) {
+      throw new WrongNumberLengthException(secret.length, userInput.length)
+    } else if (secret.length != secret.toSet.size || userInput.length != userInput.toSet.size) {
+      throw new RepeatingDigitsException()
+    } else if (secret == userInput) {
+      Correct(numTries)
+    } else {
+      val cowsCount = secret.toSet.intersect(userInput.toSet).size
+      val bullsCount = (0 until secret.length).toArray.count(index => secret(index) == userInput(index))
+      Incorrect(bullsCount, cowsCount - bullsCount)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    print("Enter your name: ")
+    print("Hi!\nEnter your name: ")
     val name = readLine()
-    println(s"Hello, $name!")
+    println(s"Hello, $name!\nLet's play. Enter secret length:")
+    val secret = generateNumberString(readLine().toInt)
+    println("Possible symbols: " ++ (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')).mkString(""))
+    println("Leave your guess:")
+    var numTries = 0
+    while (true) {
+      try {
+        val guessResult = validate(secret, readLine(), numTries)
+        numTries += 1
+        guessResult match {
+          case Incorrect(bulls, cows) =>
+            println("You got more information:")
+            println("bulls: " ++ bulls.toString ++ " and cows: " ++ cows.toString)
+            println("Try again!")
+          case _ =>
+            println("Congratulations! The secret was: " ++ secret ++ "\nPLay again!")
+            println("It took " ++ numTries.toString ++ " only steps!")
+            return
+        }
+      } catch {
+        case e: WrongNumberLengthException => println("")
+        case e: RepeatingDigitsException => println("")
+      }
+    }
   }
 }
