208242f56a015be1844451f6d40a6691ba92c1cd
a-alex-l <lesnik-a-a@yandex.ru>
2021-03-09 21:46:12 +0300

    Fixed second task. But I also have to add brackets to IntNil in test file. I hope it's ok.

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -2,20 +2,53 @@ package org.spbsu.mkn.scala
 
 import org.spbsu.mkn.scala.IntList._
 
+import scala.annotation.tailrec
+
 sealed trait IntList {
   def head: Int
   def tail: IntList
   def drop(n: Int): IntList
   def take(n: Int): IntList
   def map(f: Int => Int): IntList
-  def ::(elem: Int): IntList = ???
+  def ::(elem: Int): IntList
+}
+
+case class IntNil() extends IntList {
+  override def head: Int = undef
+  override def tail: IntList = undef
+  override def drop(n: Int): IntList = undef
+  override def take(n: Int): IntList = undef
+  override def map(f: Int => Int): IntList = this
+  override def ::(elem: Int): IntList = IntList(Seq(elem))
+}
+
+case class IntMore(seq: Seq[Int]) extends IntList {
+  override def head: Int = seq.head
+  override def tail: IntList = IntList(seq.tail)
+  override def drop(n: Int): IntList =
+    if (seq.size < n) { undef } else { IntList(seq.drop(n)) }
+  override def take(n: Int): IntList =
+    if (seq.size < n) { undef } else { IntList(seq.take(n)) }
+  override def map(f: Int => Int): IntList = IntList(seq.map(f))
+  override def ::(elem: Int): IntList = IntList(Seq(elem) ++ seq)
 }
 
 object IntList {
+  def apply(seq: Seq[Int]): IntList =
+    if (seq.isEmpty) { IntNil() } else {IntMore(seq) }
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): IntList = ???
-  def sum(intList: IntList): Int      = ???
-  def size(intList: IntList): Int     = ???
-  // extra task: implement sum using foldLeft
-  // def foldLeft(???)(???): ??? = ???
+  def fromSeq(seq: Seq[Int]): IntList = IntList(seq)
+  def sum(intList: IntList): Int =
+    intList match {
+      case IntNil() => undef
+      case _ => foldLeft ((currentSum: Int, now: Int) => currentSum + now, 0, intList)
+    }
+  def size(intList: IntList): Int = foldLeft((currentCount: Int, now: Int) => currentCount + 1, 0, intList)
+  @tailrec
+  def foldLeft(f: (Int, Int) => Int, startValue: Int, intList: IntList): Int = {
+    intList match {
+      case IntNil() => startValue
+      case _ => foldLeft(f, f(startValue, intList.head), intList.tail)
+    }
+  }
 }
--- 02/practice/src/test/scala/org/spbsu/mkn/scala/IntListTest.scala
+++ 02/practice/src/test/scala/org/spbsu/mkn/scala/IntListTest.scala
@@ -12,36 +12,36 @@ class IntListTest extends AnyFunSuite {
 
   test("tail") {
     assert(fromSeq(Seq(1,2,3)).tail == fromSeq(Seq(2,3)))
-    assert(fromSeq(Seq(1)).tail == IntNil)
+    assert(fromSeq(Seq(1)).tail == IntNil())
   }
 
   test("drop") {
     assert(fromSeq(Seq(1,2,3)).drop(0) == fromSeq(Seq(1,2,3)))
     assert(fromSeq(Seq(1,2,3)).drop(2) == fromSeq(Seq(3)))
-    assert(fromSeq(Seq(1,2,3)).drop(3) == IntNil)
+    assert(fromSeq(Seq(1,2,3)).drop(3) == IntNil())
     assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).drop(10))
   }
 
   test("take") {
-    assert(fromSeq(Seq(1,2,3)).take(0) == IntNil)
+    assert(fromSeq(Seq(1,2,3)).take(0) == IntNil())
     assert(fromSeq(Seq(1,2,3)).take(2) == fromSeq(Seq(1,2)))
     assert(fromSeq(Seq(1,2,3)).take(3) == fromSeq(Seq(1,2,3)))
     assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).take(10))
   }
 
   test("map") {
-    assert(IntNil.map(_ * 2) == IntNil)
+    assert(IntNil().map(_ * 2) == IntNil())
     assert(fromSeq(Seq(1,2,3)).map(_ * 2) == fromSeq(Seq(2,4,6)))
     assert(fromSeq(Seq(1,2,3)).map(identity) == fromSeq(Seq(1,2,3)))
   }
 
   test("size") {
-    assert(size(IntNil) == 0)
+    assert(size(IntNil()) == 0)
     assert(size(fromSeq(Seq(1,2,3))) == 3)
   }
 
   test("sum") {
-    assertThrows[UnsupportedOperationException](sum(IntNil))
+    assertThrows[UnsupportedOperationException](sum(IntNil()))
     assert(sum(fromSeq(Seq(1,2,3))) == 6)
     assert(sum(fromSeq(Seq(1))) == 1)
   }
