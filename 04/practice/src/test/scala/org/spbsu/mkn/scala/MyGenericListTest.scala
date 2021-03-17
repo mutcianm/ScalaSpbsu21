9bef1c487c8a5e8ccc007ae17af8ab8796ec9a93
Egor Postnikov <ea-post134@yandex.ru>
2021-03-17 19:09:45 +0300

    Sort implementation

---
+++ 04/practice/src/test/scala/org/spbsu/mkn/scala/MyGenericListTest.scala
@@ -0,0 +1,59 @@
+package org.spbsu.mkn.scala
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.spbsu.mkn.scala.MyGenericList.{fromSeq, size, sort, sum}
+
+class MyGenericListTest extends AnyFunSuite {
+
+  test("head") {
+    val a = fromSeq(Seq(1, 2, 3))
+    assert(a.head == 1)
+    assert(fromSeq(Seq(1)).head == 1)
+    assertThrows[UnsupportedOperationException](fromSeq(Seq()).head)
+  }
+
+  test("tail") {
+    assert(fromSeq(Seq(1, 2, 3)).tail == fromSeq(Seq(2, 3)))
+    assert(fromSeq(Seq(1)).tail == MyNil)
+  }
+
+  test("drop") {
+    assert(fromSeq(Seq(1, 2, 3)).drop(0) == fromSeq(Seq(1, 2, 3)))
+    assert(fromSeq(Seq(1, 2, 3)).drop(2) == fromSeq(Seq(3)))
+    assert(fromSeq(Seq(1, 2, 3)).drop(3) == MyNil)
+    assertThrows[UnsupportedOperationException](fromSeq(Seq(1, 2, 3)).drop(10))
+  }
+
+  test("take") {
+    assert(fromSeq(Seq(1, 2, 3)).take(0) == MyNil)
+    assert(fromSeq(Seq(1, 2, 3)).take(2) == fromSeq(Seq(1, 2)))
+    assert(fromSeq(Seq(1, 2, 3)).take(3) == fromSeq(Seq(1, 2, 3)))
+    assertThrows[UnsupportedOperationException](fromSeq(Seq(1, 2, 3)).take(10))
+  }
+
+  test("map") {
+    assert(MyNil.map((i: Int) => i * 2) == MyNil)
+    assert(fromSeq(Seq(1, 2, 3)).map((i: Int) => i * 2) == fromSeq(Seq(2, 4, 6)))
+    assert(fromSeq(Seq(1, 2, 3)).map(identity[Int]) == fromSeq(Seq(1, 2, 3)))
+  }
+
+  test("size") {
+    assert(size(MyNil) == 0)
+    assert(size(fromSeq(Seq(1, 2, 3))) == 3)
+  }
+
+  test("sum") {
+    assertThrows[UnsupportedOperationException](sum(MyNil))
+    assert(sum(fromSeq(Seq(1, 2, 3))) == 6)
+    assert(sum(fromSeq(Seq(1))) == 1)
+  }
+
+
+  test("sort") {
+    assert(sort(fromSeq(Seq(4, 3, 2, 1))) == fromSeq(Seq(1, 2, 3, 4)))
+    assert(sort(fromSeq(Seq(2 -> 3, 2 -> 1))) == fromSeq(Seq(2 -> 1, 2 -> 3)))
+    assert(sort(fromSeq(Seq(2 -> 3, 2 -> 1, 1 -> 5))) == fromSeq(Seq(1 -> 5, 2 -> 1, 2 -> 3)))
+    assert(sort(fromSeq(Seq(3 -> 2, 2 -> 1, 1 -> 5))) == fromSeq(Seq(1 -> 5, 2 -> 1, 3 -> 2)))
+  }
+
+}
