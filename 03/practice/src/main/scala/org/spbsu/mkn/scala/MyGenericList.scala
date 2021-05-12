4c0c11a5c5d3e72b9b7be05d67822d6ffc17bce8
a-alex-l <lesnik-a-a@yandex.ru>
2021-05-13 00:19:55 +0300

    Changed MyNil from case class to case object. Deleted function sumOfTwoNumbers by changing sum type.

--- 03/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
+++ 03/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
@@ -4,21 +4,21 @@ import org.spbsu.mkn.scala.MyGenericList._
 import scala.annotation.tailrec
 import scala.Numeric
 
-sealed trait MyGenericList[T] {
+sealed trait MyGenericList[+T] {
   def head: T
   def tail: MyGenericList[T]
   def drop(n: Int): MyGenericList[T]
   def take(n: Int): MyGenericList[T]
   def map[K](f: T => K): MyGenericList[K]
-  def ::(elem: T): MyGenericList[T] = TMore(elem, this)
+  def ::[K >: T](elem: K): MyGenericList[K] = TMore(elem, this)
 }
 
-case class MyNil[T]() extends MyGenericList[T] {
-  override def head: T = undef
-  override def tail: MyGenericList[T] = undef
-  override def drop(n: Int): MyGenericList[T] = if (n == 0) { this } else { undef }
-  override def take(n: Int): MyGenericList[T] = if (n == 0) { this } else { undef }
-  override def map[K](f: T => K): MyGenericList[K] = MyNil[K]()
+case object MyNil extends MyGenericList[Nothing] {
+  override def head: Nothing = undef
+  override def tail: MyGenericList[Nothing] = undef
+  override def drop(n: Int): MyGenericList[Nothing] = if (n == 0) { this } else { undef }
+  override def take(n: Int): MyGenericList[Nothing] = if (n == 0) { this } else { undef }
+  override def map[K](f: Nothing => K): MyGenericList[K] = fromSeq[K](Seq())
 }
 
 case class TMore[T](value: T, prev: MyGenericList[T]) extends MyGenericList[T] {
@@ -27,31 +27,26 @@ case class TMore[T](value: T, prev: MyGenericList[T]) extends MyGenericList[T] {
   override def drop(n: Int): MyGenericList[T] =
     if (n == 0) { this } else { prev.drop(n - 1) }
   override def take(n: Int): MyGenericList[T] =
-    if (n == 0) { MyNil() } else { TMore(value, prev.take(n - 1)) }
+    if (n == 0) { MyNil.asInstanceOf[MyGenericList[T]] } else { TMore(value, prev.take(n - 1)) }
   override def map[K](f: T => K): MyGenericList[K] = TMore(f(value), prev.map(f))
 }
 
 object MyGenericList {
-  private def sumOfTwoNumbers[T](first: T, second: T)(implicit n: Numeric[T]) : T = {
-    import n.mkNumericOps
-    first + second
-  }
   def apply[T](seq: Seq[T]): MyGenericList[T] =
-    if (seq.isEmpty) { MyNil() } else { TMore(seq.head, MyGenericList(seq.tail)) }
+    if (seq.isEmpty) { MyNil.asInstanceOf[MyGenericList[T]] } else { TMore(seq.head, MyGenericList(seq.tail)) }
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
   def fromSeq[T](seq: Seq[T]): MyGenericList[T] = MyGenericList(seq)
   def size[T](TList: MyGenericList[T]): Int = foldLeft((currentCount: Int, now: T) => currentCount + 1, 0, TList)
-  def sum[T](TList: MyGenericList[T])(implicit n: Numeric[T]): T =
+  def sum(TList: MyGenericList[Int]): Int =
     TList match {
-      case MyNil() => undef
-      case _ => foldLeft((currentSum: T, now : T) => sumOfTwoNumbers(now, currentSum), TList.head, TList.tail) // Scala is broken
+      case MyNil => undef
+      case _ => foldLeft((currentSum: Int, now : Int) => now + currentSum, TList.head, TList.tail) // Scala is broken
     }
   @tailrec
   def foldLeft[T, K](f: (K, T) => K, startValue: K, TList: MyGenericList[T]): K = {
     TList match {
-      case MyNil() => startValue
-      case _ =>
-        foldLeft(f, f(startValue, TList.head), TList.tail)
+      case MyNil => startValue
+      case _ => foldLeft(f, f(startValue, TList.head), TList.tail)
     }
   }
 }
--- 03/practice/src/test/scala/org/spbsu/mkn/scala/MyGenericListTest.scala
+++ 03/practice/src/test/scala/org/spbsu/mkn/scala/MyGenericListTest.scala
@@ -14,36 +14,36 @@ class MyGenericListTest extends AnyFunSuite {
 
   test("tail") {
     assert(fromSeq(Seq(1,2,3)).tail == fromSeq(Seq(2,3)))
-    assert(fromSeq(Seq(1)).tail == MyNil())
+    assert(fromSeq(Seq(1)).tail == MyNil)
   }
 
   test("drop") {
     assert(fromSeq(Seq(1,2,3)).drop(0) == fromSeq(Seq(1,2,3)))
     assert(fromSeq(Seq(1,2,3)).drop(2) == fromSeq(Seq(3)))
-    assert(fromSeq(Seq(1,2,3)).drop(3) == MyNil())
+    assert(fromSeq(Seq(1,2,3)).drop(3) == MyNil)
     assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).drop(10))
   }
 
   test("take") {
-    assert(fromSeq(Seq(1,2,3)).take(0) == MyNil())
+    assert(fromSeq(Seq(1,2,3)).take(0) == MyNil)
     assert(fromSeq(Seq(1,2,3)).take(2) == fromSeq(Seq(1,2)))
     assert(fromSeq(Seq(1,2,3)).take(3) == fromSeq(Seq(1,2,3)))
     assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).take(10))
   }
 
   test("map") {
-    assert(MyNil[Int]().map(_ * 2) == MyNil())
+    assert(MyNil.asInstanceOf[MyGenericList[Int]].map(_ * 2) == MyNil)
     assert(fromSeq(Seq(1,2,3)).map(_ * 2) == fromSeq(Seq(2,4,6)))
     assert(fromSeq(Seq(1,2,3)).map(identity) == fromSeq(Seq(1,2,3)))
   }
 
   test("size") {
-    assert(size(MyNil()) == 0)
+    assert(size(MyNil) == 0)
     assert(size(fromSeq(Seq(1,2,3))) == 3)
   }
 
   test("sum") {
-    assertThrows[UnsupportedOperationException](sum(MyNil[Int]()))
+    assertThrows[UnsupportedOperationException](sum(MyNil))
     assert(sum(fromSeq(Seq(1,2,3))) == 6)
     assert(sum(fromSeq(Seq(1))) == 1)
   }
